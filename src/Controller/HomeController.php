91cb8a5b9032692cc494175ae3bef29e6ef94c8a
OmarBnr1 <amrouchbnr@gmail.com>
2021-03-04 01:18:05 +0100

    Podcast Donut PodTasty

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
->>>>>>> 3916562c87f0ab4408ef3109ef190af452e666e0
+//>>>>>>> 3916562c87f0ab4408ef3109ef190af452e666e0
 
 class HomeController extends AbstractController
 {
--- src/Controller/PodcastController.php
+++ src/Controller/PodcastController.php
@@ -3,13 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Podcast;
-use App\Form\PodcastFormType;
+use App\Form\PodcastType;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\PodcastRepository;
-use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormTypeInterface;
 
 class PodcastController extends AbstractController
 {
@@ -22,66 +26,87 @@ class PodcastController extends AbstractController
             'controller_name' => 'PodcastController',
         ]);
     }
-
     /**
      * @param PodcastRepository $repository
      * @return Response
-     * @Route("/AffichPodcast",name="AffichePodcast")
+     * @Route("/AffichPodcast" , name="AffichePodcast")
      */
     public function Affiche(PodcastRepository $repository){
-      $repo=$this->getDoctrine()->getRepository(Podcast::class);
-      $Podcast=$repo->findAll();
-      return $this->render('/podcast/Affiche.html.twig',['podcast'=>$Podcast]);
+        $repo=$this->getDoctrine()->getRepository(Podcast::class);
+        $Podcast=$repo->findAll();
+        return $this->render('/podcast/Affiche.html.twig',['podcast'=>$Podcast]);
     }
-
     /**
-     * @Route("SuppPodcast")
-     * @param $id
+     * @Route("/SuppPodcast/{id}" , name="SuppPodcast")
      */
     public function Delete($id, PodcastRepository $repository){
         $Podcast=$repository->find($id);
         $em=$this->getDoctrine()->getManager();
         $em->remove($Podcast);
         $em->flush();
-        return $this->redirectToRoute("Affiche.html.twig");
+        return $this->redirectToRoute("AffichePodcast");
     }
+        /**
+         * @Route("Podcast/Add")
+         */
+        function Add(Request $request)
+        {
+            $Podcast = new Podcast();
+            $form = $this->createForm(PodcastType::class, $Podcast);
+            $form->add('Ajouter', SubmitType::class);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                /**
+                 * @var UploadedFile $file
+                 */
+                //$file=$Podcast->getPodcastImage();
+                $file = $form->get('PodcastImage')->getData();
+
+                $fileName = md5(uniqid()).'.'.$file->guessExtension();
+                $file->move(
+                    $this->getParameter('kernel.project_dir'),$fileName
+                );
+                $Podcast->setPodcastImage($fileName);
+                $em = $this->getDoctrine()->getManager();//->persist($form->getData());
+
+              //  $em=$this->getDoctrine()->getManager()->flush();
+
+                $em->persist($Podcast);
+                $em->flush();
+                return $this->redirectToRoute('AffichePodcast');
+            }
+            return $this->render('Podcast/Add.html.twig', [
+                'form' => $form->createView()]);
+        }
 
     /**
-     * @param \Symfony\Component\HttpFoundation\Request $request
-     * @return Response
-     * @Route("/AddPodcast")
+     * @Route ("Podcast/Update/{id}",name="UpdatePodcast")
      */
-     function add(\Symfony\Component\HttpFoundation\Request $request){
-        $Podcast= new Podcast();
-        $form=$this->createForm(PodcastFormType::class,$Podcast);
-         $form->add('Ajouter',SubmitType::class);
-        $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-            $em=$this->getDoctrine()->getManager();
-            $em->persist($Podcast);
-            $em->flush();
-            //return $this->redirectToRoute('AffichePodcast');
+        function update(PodcastRepository $repository,$id,Request $request){
+            $Podcast=$repository->find($id);
+            $form = $this->createForm(PodcastType::class);
+            $form->add('Update',SubmitType::class);
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid()) {
+                /**
+                 * @var UploadedFile $file
+                 */
+                $file = $form->get('PodcastImage')->getData();
+                $fileName = md5(uniqid()).'.'.$file->guessExtension();
+                $file->move(
+                    $this->getParameter('kernel.project_dir'),$fileName
+                );
+                $Podcast->setPodcastImage($fileName);
+
+                $em=$this->getDoctrine()->getManager();
+                $em->flush();
+                return $this->redirectToRoute('AffichePodcast');
+            }
+            return $this->render('Podcast/Update.html.twig',[
+                'f'=>$form->createView()]);
+
         }
-        return $this->render('Podcast/Add.html.twig',[
-            'form'=>$form->createView()]);
-     }
-     /**
-      * @Route ("Podcast/Update/{id}",name="UpdatePodcast"
-      */
-     /*function update(PodcastRepository $repository, $id,\Symfony\Component\HttpFoundation\Request $request){
-         $Podcast=$repository->find($id);
-         $form=$this->createForm(PodcastFormType::class,$Podcast);
-         $form->add('Update',SubmitType::class);
-         $form->handleRequest($request);
-         if($form->isSubmitted() && $form->isValid()) {
-             $em = $this->getDoctrine()->getManager();
-             $em->flush();
-             return $this->redirectToRoute('AffichePodcast');
-         }
-         return $this->render('Podcast/Update.html.twig',[
-             'form'=>$form->createView()]);
 
 
-     }
-*/
+
 }
