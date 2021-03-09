63f33c299390cd080a704013b361c9856a074621
OmarBnr1 <amrouchbnr@gmail.com>
2021-03-09 11:42:46 +0100

    Tasty Donut Podcast PodTasty <3 <3

---
+++ src/Form/ReclamationType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Reclamation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ReclamationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Type')
+            ->add('Description')
+            ->add('Status')
+            ->add('PodcastId')
+            ->add('UserId')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Reclamation::class,
+        ]);
+    }
+}
